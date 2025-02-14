245cb8157ef46fec714779f3db65e3d8a91149e4
christian endale <230035665@aston.ac.uk>
2025-02-14 16:49:41 +0000

    partially completed lab4

--- lab4-primit/java/ErrorsEnum.java
+++ lab4-primit/java/ErrorsEnum.java
@@ -4,7 +4,6 @@ import java.util.EnumSet;
 public class ErrorsEnum
 {
     enum Error { FP_ROUNDING, FP_OVERFLOW, FP_UNDERFLOW, INT_OVERFLOW }
-
     enum Result { A_BIT_DIFFERENT, INFINITY, ZERO, VERY_DIFFERENT }
     
     private static <E extends Enum<E>> E getEnumElement(String elementTypeName, Class<E> elementType)
@@ -32,23 +31,20 @@ public class ErrorsEnum
         return result;
     }
   
-    private static Result error2Result(Error e)
+    private static Error result2Error(Result r) 
     {
         Result result = null;
         
         switch (e) {
-        case FP_ROUNDING:
-            result = Result.A_BIT_DIFFERENT;
-            break;
-        case FP_OVERFLOW:
-            result = Result.INFINITY;
-            break;
-        case FP_UNDERFLOW:
-            result = Result.ZERO;
-            break;
-        case INT_OVERFLOW:
-            result = Result.VERY_DIFFERENT;
-            break;
+        case A_BIT_DIFFERENT:
+            return Error.FP_ROUNDING;
+        case INFINITY:
+            return Error.FP_OVERFLOW;
+        case ZERO:
+            return Error.FP_UNDERFLOW;
+        case VERY_DIFFERENT:
+            return Error.INT_OVERFLOW;
+        break;
         }
         
         return result;
@@ -57,13 +53,13 @@ public class ErrorsEnum
     public static void main(String[] args)
     {
         System.out.print("Known errors = ");
-        for (Error e : EnumSet.allOf(Error.class)) 
+        for (Result r : EnumSet.allOf(Result.class)) 
         {
             System.out.print(e + " ");
         }
         System.out.println();
         
-        Error e = getEnumElement("error", Error.class);
-        System.out.println(e + " results in: " + error2Result(e));
+        Result r = getEnumElement("Result", Result.class);
+        System.out.println(r + " results in: " + result2Error(e));
     }
 }
