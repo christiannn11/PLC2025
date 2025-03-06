5a150f409bf1d523cdc58373109d833a8f13a6d2
christian endale <230035665@aston.ac.uk>
2025-03-06 12:26:18 +0000

    partially finished lab6

--- lab6-collections/haskell/playlistColls.hs
+++ lab6-collections/haskell/playlistColls.hs
@@ -54,7 +54,7 @@ playlist2 = [piece2, advert1]
 
 playlists = [playlist1, playlist2] -- a list of lists
 
-adsFromPlaylists = "todo" -- TASK 6.4(b)
+adsFromPlaylists = [item_length_secs item | item <- playlist1, item_length_secs item < 20] -- TASK 6.4(b)
 
 shortItemLenghts1 = "todo" -- TASK 6.4(a)
 
